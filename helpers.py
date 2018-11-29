923f25b1cd0b024bae02063feecaebba22f1f938
squidmania <squidman@gmail.com>
2018-11-29 21:43:29 +0000

    Add parse function to mung data Relates #2 #5

--- helpers.py
+++ helpers.py
@@ -8,5 +8,18 @@ def google_book_search(search, startIndex=0, maxResults=20):
     return {"status": r.status_code, "body": r.json()}
 
 
-def parse_search(item, results):
-    return "Hello"
+def parse_search(json):
+    if json["status"] != 200:
+        return {"items": []}
+    body = json["body"]
+    book_list = []
+    for i in body["items"]:
+        book = {}
+        for field in ["authors", "title", "publisher", "imageLinks"]:
+            try:
+                book[field] = i["volumeInfo"][field]
+            except KeyError:
+                book[field] = ""
+        book_list.append(book)
+
+    return {"items": book_list}
