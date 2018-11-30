1549c83323770926679eb26534f01d0ca859c295
squidmania <squidman@gmail.com>
2018-11-30 01:40:14 +0000

    add basic prevnext (needs refactor before PR) Relates #14

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from helpers import google_book_search, parse_search
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,14 +10,20 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/query', methods=['POST'])
+@app.route('/query')
 def query():
-    if request.args.get('start') is None or request.args.get('start') < 0:
+    if request.args.get('search') is None:
+        return redirect(url_for("/"))
+
+    if request.args.get('start') is None or int(request.args.get('start')) < 0:
         start = 0
-    query = request.form['search']
+    else:
+        start = int(request.args.get('start'))
+
+    query = request.args.get('search')
     api_search = google_book_search(query, start)
     parsed_text = parse_search(api_search)
-    return render_template("search.html", search=query, data=parsed_text)
+    return render_template("search.html", start=start+1, count=start + len(parsed_text["items"]), search=query, data=parsed_text)
 
 
 @app.errorhandler(404)
--- helpers.py
+++ helpers.py
@@ -10,16 +10,19 @@ def google_book_search(search, startIndex=0, maxResults=20):
 
 def parse_search(json):
     if json["status"] != 200:
-        return {"items": []}
+        return {"total": 0, "items": []}
     body = json["body"]
     book_list = []
-    for i in body["items"]:
-        book = {}
-        for field in ["authors", "title", "publisher", "imageLinks"]:
-            try:
-                book[field] = i["volumeInfo"][field]
-            except KeyError:
-                book[field] = ""
-        book_list.append(book)
+    try:
+        for i in body["items"]:
+            book = {}
+            for field in ["authors", "title", "publisher", "imageLinks"]:
+                try:
+                    book[field] = i["volumeInfo"][field]
+                except KeyError:
+                    book[field] = ""
+            book_list.append(book)
+    except KeyError:
+        return {"total": 0, "items": []}
 
     return {"index": 0, "total": body["totalItems"], "items": book_list}
