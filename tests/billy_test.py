f1afcad3a68c060f98dbbde327214aef8b9e75d9
squidmania <squidman@gmail.com>
2019-01-06 17:57:50 +0000

    add basic integration tests for app elements relates #24

--- tests/billy_test.py
+++ tests/billy_test.py
@@ -8,11 +8,28 @@ def test_home_route(client):
         assert response.status_code == 200
 
 
+def test_home_route_renders(client):
+    with client as c:
+        response = c.get("/")
+        assert b'<title>Billy Booksearch</title>' in response.data
+        assert b'alt="bookcase"' in response.data
+        assert b'id="search"' in response.data
+        assert b'id="search_button"' in response.data
+
+
 def test_search_route(client):
     with client as c:
         _response = c.get("/query?search=Harry+Potter")
         assert request.path == '/query'
         assert request.args['search'] == 'Harry Potter'
+
+
+def test_search_route_renders(client):
+    with client as c:
+        response = c.get("/query?search=Harry+Potter")
+        assert b'id="search"' in response.data
+        assert b'id="prev"' in response.data
+        assert b'id="next"' in response.data
 
 
 def test_empty_search_is_redirected(client):
